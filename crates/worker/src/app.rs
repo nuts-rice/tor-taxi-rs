4a0c5cc89a7d89de6fb14c5ff01b2916811ad8c3
nuts_rice <red44443@gmail.com>
2026-09-15 17:35:46 -0600

    feat(web): remove autohydration

--- crates/worker/src/app.rs
+++ crates/worker/src/app.rs
@@ -18,8 +18,6 @@ pub fn shell(options: LeptosOptions) -> impl IntoView {
             <head>
                 <meta charset="utf-8"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <AutoReload options=options.clone() />
-                <HydrationScripts options/>
                 <MetaTags/>
             </head>
             <body>
